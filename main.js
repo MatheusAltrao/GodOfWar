93a99d1078bd0c2ad88dba58eea15d929c4980d4
matheus <matheusaltrao2@gmail.com>
2022-11-28 21:42:41 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    530: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+
+  },
+});
+
+
+
+
+
